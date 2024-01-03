774f7e512f1033c73d6f871202c976d19b4eb5ef
anderslatif <anderslatif@users.noreply.github.com>
2024-01-03 13:58:15 +0100

    Updated the readme course files

--- 00._Course_Material/About_This_Course.md
+++ 00._Course_Material/About_This_Course.md
@@ -1,36 +1,89 @@
-Introducing the tools
-Teams: Communication and hand-ins.
-This Repo: Code, slides, assignment descriptions. 
-…
+## Introducing the tools
+
+**This Repo**: Code, semester plan, assignment descriptions. 
+**Teams**: Communication and hand-ins.
+**Google Docs**: Contains slides and diagrams.  
+
+
 Feel free to make PRs. This is the reason why I’ve defined a lot of the material in Git.
-…
-Bred viden om mange ting (modsat dyb viden om få ting). 
-Tool box 
-Github CoPilot
-% live coding
-The basics for every topic:
-What is it? What is the theory behind it?
-What does it aim to solve?
-How is it an improvement over other things (pros)?
-What complications and downsides does it introduce (cons)? 
-For which use cases would it be a good choice?
+
+## The Slides
+
+In the next iteration of the course the slides will be generated in markdown and placed in the repository. 
+
+The idea is that the slides shouldn't be informative but contain inspiration for class discussion. 
+
+## System Integration (SI) vs. DLS
+
+The idea is that DLS will be about developing large systems and having these systems running successfully in production. SI is about zooming in on where different type of integration works. 
+
+DLS will be about creating a single cohesive large project throughout the semester and defending it during the exam. 
+
+SI will be about creating multiple stripped down code examples that show how different types of integration work. 
+
+The only exception is that I will teach cloud with Azure on behalf of DLS. 
+
+## The overall learning goals in SI
+
+The goal is to give you broad knowledge about different technologies, tools, protocols, system constellation. By the end of the course you should feel like you have a tool box and the learning goal is being able to select the correct tool for the job. 
+
+That's why, for each integration you should be able to know the use and downside of the choice. 
+
+For all topics you should be able to at least answer these questions:
+
+* What is it? What is the theory behind it?
+* What does it aim to solve?
+* How is it an improvement over other things (pros)?
+* What complications and downsides does it introduce (cons)? 
+* For which use cases would it be a good choice?
+
 This isn’t all there is to know but if you can answer these things then you have a good foundation.
-…
-…
-Reflection level, ramme for uddannelsen, ability to find information yourselves rather than currated content
-…
-Learning goals: They will tell you how much you are expected to know about a topic and in what way. 
-…
+
+Note: Everything in your education so far has been about REST API. Now it's about everything but. There is nothing wrong with REST APIs but the assumption is that you know them really well by now. 
+
+## The structure surrounding the lectures
+
+#### Prior to the class
+
+The topics are known in advance. At this educational level you are expected to know how to gather information and evaluate their correctness rather than only engaging with curated material. Create thorough notes. 
+
+Every week there will be a quiz. Take the quiz after you are done researching to evaluate your understanding of the topic. Update the notes after. 
+
+#### During the class
+
+Be engaged and join the class discussions!
+
 Why research the topics if we are going to talk about them in class anyway?
 - You will be able to develop your own metaphors and ways to understand the course material first.
 - You get to process the material multiple times. This shortens the time you need to review the topics prior to the exam. 
-- You can participate in class. Saying things out loud in front of people helps you remember the material. 
-…
-Why both Node.js and Python.
+- You can have nice discussions when there is pair discussions. 
+- You can participate in class. Saying things out loud in front of people will help you remember the topic. Everyone should aim to at least say one thing each week. 
 
-Contrast and compare. Helps point out what is the core of the topic rather than just syntax and language specific implementation. 
-You are allowed to create your own examples in other languages. 
-.. 
-The exam, you are allowed to come to the exam with my code but you still need to understand it. Based on limited data I can state that students that create their own version of the code have performed better during the code part of the exam. 
+###### Github CoPilot
 
-https://docs.google.com/document/d/1vJu2XiBDSuuFPt5j70RdFc6ERA4HEl0eTmqq3sL1sR0/edit?usp=sharing
+We will use it during class. Feel free to enable it or not. The problem of using it for live coding is that it knows my code well and will suggest code different from you. Shout out during class if I should pause the live coding.  
+
+#### After the class
+
+Reflect on the learned content, go through the slides and update your notes. Having these notes will make it easy to read up for the exam. 
+
+## The overall structure of the semester
+
+The ideal effort put into the course if you prepare continuously. Note that the top part should be non-stressful effort:
+
+[Semester work load](./00._Course_Material/semester_work_load.png)
+
+
+## Multiple programming languages
+
+In class we will use both Node.js and Python. But why?
+
+The goal of the course is to understand integration from a language agnostic view but completely removing coding from the lectures would make everything too vague. 
+
+By showing solutions in two languages the aim is for us to be able to contrast and compare the solutions. By abstracting the syntax and language specific implementation what should remain is an understanding of the core integration parts. 
+
+For the exam you are welcome  to create your own examples in other languages. 
+
+## The exam
+
+### TODO (Next Week)
---
+++ 00._Course_Material/markdown_table_generator.py
@@ -0,0 +1,211 @@
+# todo add Google Drive link to slides and Github link in Teams
+table_data = [
+    {
+        "Title": "Introduction + Data Formats",
+        "Date": "Feb. 1st", 
+        "Description": """
+            - Data encoding 
+
+            - Hashing, serializing, marshalling
+
+            - Packages eco system in Node and Python
+
+            - REST APIs
+        """,
+        "Learning Goals": """
+
+            - Can argue about different build tools for Python and understands their pros and cons. 
+
+            - Can use poetry. 
+
+            - Understands encoding, serializing and marshalling. Knows about charsets and encoding types.
+        """,
+        "Resources and Activities": [
+            "[01a [Individual] File formats bonanza!](00._assignments/01._Introduction_Data_Formats/01._[Individual]_File_formats_bonanza!.md)",
+            "[[Individual] Azure for Students](00._assignments/01._Introduction_Data_Formats/00._[Individual]_Azure_For_Students.md)",
+          
+        ]
+    },
+    {
+        "Title": "Data",
+        "Date": "Feb. 8th", 
+        "Description": """
+            - Intro / This course
+
+            - Environment Setup
+
+            - File types and data formats.
+
+            - Python Modules
+
+            - Starting with Node and Express
+
+            = Weekly Grubler: Cheap static deployment.
+        """,
+        "Learning Goals": """
+            - Alternatives to REST API.
+
+            - Data formats. TXT, JSON, YAML, CSV, XML
+
+            - Can parse files in these formats after the homework.
+
+            - Can run files with Node and Python.
+
+            - Can work with modules in Python / Javascript (frontend and backend).
+        """,
+        "Resources and Activities": [
+            "[[02a [Individual] Data format translation servers - Part II](00._assignments/02._Data/02a._[Individual]_Data_format_translation_servers_-_Part_II.md)",
+            "[[Optional] Serialize / Marshall data](00._assignments/02._Data/00._[Optional]_Serialize__Marshall_data.md)",
+          
+        ]
+    },
+    {
+        "Title": "Server to server / Real-time communication - Part I",
+        "Date": "Feb. 15th", 
+        "Description": """
+            - Date
+
+            - Communication
+
+            - Making a server act as a client to another server.
+
+            - ngrok 
+
+            - Environment Variables (OS level, Runtime environment level)
+        """,
+        "Learning Goals": """
+            - Understand how date affects systems and communication between services.
+
+            - Understands challenges and edge cases when working with date and why this is important during integration.
+
+            - Knows about different communication protocols on a superficial level.
+
+            - Knows how to make a HTTP requests between languages. 
+
+            - Can use ngrok
+
+            - Understand the different levels of environment variables and how they differ. 
+
+            - Knows how to define environment variables in different languages.
+        """,
+        "Resources and Activities": [
+            "03. [Pair] Database granular access](00._assignments/03._Server_to_server_Real-time_communication_-_Part_I/03a._[Pair]_Database_granular_access.md)",          
+        ]
+    },
+    {
+        "Title": "Documentation",
+        "Date": "Feb. 22nd", 
+        "Description": """
+            - Backing up MySQL
+
+            - Documenting MySQL 
+
+            - OpenAPI
+
+            - Swagger + Express/FastAPI/Spring
+
+            - Architecture Diagrams
+
+            - Azure Virtual Machines
+        """,
+        "Learning Goals": """
+            - Can document and backup MySQL
+
+            - Has considered how to document the databases they are familiar with.
+
+            - Understands the what and why of OpenAPI
+
+            - Can generate OpenAPI documentation in their favorite(s) frameworks.
+
+            - Can do architecture diagrams in their favorite tool.
+
+            - Can create virtual machines without aid on your favorite hosting platform.
+        """,
+        "Resources and Activities": [
+            "04a [Individual] Create OpenAPI Documentation](00._assignments/04._Documentation/04a._[Individual]_Create_OpenAPI_Documentationss.md)", 
+            "[Resource: Video on OpenAPI](https://www.youtube.com/watch?v=pRS9LRBgjYg)"         
+        ]
+    },
+    {
+        "Title": "Real-time communication - Part II",
+        "Date": "Feb. 29th", 
+        "Description": """
+            - Client polling, short polling, long polling.
+
+            - Server-sent events
+
+            - Webhooks
+        """,
+        "Learning Goals": """
+            - Understands client polling, short polling, long polling and the pros and cons of each approach.
+
+            - Understands SSE and can explain how to implement it. Has an example ready. 
+
+            - Understands the pros and cons of SSE and can compare it to Websockets. 
+
+            - Can recall how we did Github Wehooks without having to be able to do it on the fly. Can illustrate exactly how it works by drawing a diagram.
+
+            - Understands how to approach building a custom webhook system.
+        """,
+        "Resources and Activities": [
+            "05a [Individual] SSE example](00._assignments/05._Real-time_communication_-_Part_II/05a._[Individual]_SSE_example.md)", 
+            "05b [Pair] Expose and integrate with a webhook system](00._assignments/05._Real-time_communication_-_Part_II/05b._[Pair]_Expose_and_integrate_with_a_webhook_system.md", 
+        ]
+    },
+    {
+        "Title": "Cloud",
+        "Date": "March 7th", 
+        "Description": """
+                Azure:
+                - Virtual Machines
+                - Blob Storage
+                - Functions
+
+                Terraform
+        """,
+        "Learning Goals": """
+            - Can create serverless functions (AWS Lambda, Azure Functions, Vercel etc).
+        """,
+        "Resources and Activities": [
+        ]
+    },
+    {
+        "Title": "No Lecture",
+        "Date": "March 14th", 
+        "Description": """
+            Work on the homework. 
+        """,
+        "Learning Goals": """
+        """,
+        "Resources and Activities": [
+        ]
+    },
+]
+
+
+def generate_markdown_table(data):
+    headers = data[0].keys()
+
+    header_row = "| " + " | ".join(headers) + " |"
+    separator_row = "| " + " | ".join(["---"] * len(headers)) + " |"
+
+    table_rows = [header_row, separator_row]
+
+    for row in data:
+        row_values = []
+        for key in headers:
+            if key == "Resources and Activities":
+                resources_str = "<br><br>".join(row[key])
+                row_values.append(resources_str)
+            else:
+                formatted_value = row[key].replace("\n", "<br>")
+                row_values.append(formatted_value)
+        row_str = "| " + " | ".join(row_values) + " |"
+        table_rows.append(row_str)
+
+    return "\n".join(table_rows)
+
+
+markdown_table = generate_markdown_table(table_data)
+
+print(markdown_table)
