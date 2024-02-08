d997917a71d194d286d8c67e4cbd0e6d161817fb
anderslatif <anderslatif@users.noreply.github.com>
2024-02-08 10:35:17 +0100

    Finished with encoding and decoding examples

---
+++ 04._Encoding_Decoding/node/index.js
@@ -0,0 +1,10 @@
+const base64EncodedString = btoa("hello there, this is base 64");
+
+console.log(base64EncodedString);
+
+const base64DecodedString = atob(base64EncodedString);
+
+console.log(base64DecodedString);
+
+
+
---
+++ 04._Encoding_Decoding/python/main.py
@@ -0,0 +1,7 @@
+encoded_string = "hallå".encode()
+
+print(encoded_string)
+
+decoded_string = encoded_string.decode()
+
+print(decoded_string)
