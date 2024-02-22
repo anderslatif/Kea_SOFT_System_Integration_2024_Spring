d66704621ff64536315ba09084b6070b233f42b4
anderslatif <anderslatif@users.noreply.github.com>
2024-02-22 10:55:10 +0100

    Shared the short polling example

---
+++ 11._Short_Polling/node/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("public"));
+
+const randomNumbers = [];
+
+
+app.get("/randomNumbers", (req, res) => {
+    res.send({ data: randomNumbers });
+});
+
+app.post("/simulateNewRandomNumbers", (req, res) => {
+    const newNumber = getRandomInt(3, 1001);
+    randomNumbers.push(newNumber);
+    res.send({ data: newNumber })
+});
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const PORT = 8080;
+app.listen(PORT, () => console.log("Server is running on port", PORT));
